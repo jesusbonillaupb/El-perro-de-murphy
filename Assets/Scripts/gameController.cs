7e71894b75d30c5f1e6b3d251b84f03d5219be7e
jesusbonillaupb <80067532+jesusbonillaupb@users.noreply.github.com>
2024-11-07 02:00:51 -0500

    version final 1

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class gameController : MonoBehaviour
 {
@@ -37,5 +38,14 @@ public class gameController : MonoBehaviour
 
     public void Exit(){
         Application.Quit();
+    }
+    public void Quit()
+    {
+
+        Time.timeScale = 1;
+        pausePlay.SetActive(true);
+        isPaused = false;
+        SceneManager.LoadScene("MainMenu");
+
     }
 }
